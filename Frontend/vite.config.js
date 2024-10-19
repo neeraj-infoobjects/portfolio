5a1ad8f8f31f6061fd94b9e27cf48836f0b32b32
neeraj-infoobjects <neeraj.borana@infoobjects.com>
2024-10-19 15:57:09 +0530

    six-commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // This allows external access
+    port: 5173, // Make sure to specify the port if needed
+  },
+});
